6bca4f0ec29e3f1cbfd4da5372ec6c79eff91870
dchotzen-hartzell <dchotzen-hartzell19@punahou.edu>
2016-09-17 14:08:01 -1000

    Test commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Team 5159's test TeleOP
+ */
+
+@TeleOp(name = "TeleOp", group= "Test") @Disabled
+public class TestTeleOp extends OpMode {
+
+    @Override
+    public void init() {}
+
+    @Override
+    public void loop() {
+
+
+
+    }
+
+
+}
